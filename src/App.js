296846212b6022315037e4c2d74c1c4535d3bbef
Lalit <choudharylalit182@gmail.com>
2023-03-07 00:58:08 +0530

    added browse entertainment page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Registration from './Pages/Registration/Registration';
 import Category from './Pages/Category/Category';
 import Home from './Pages/Home/Home';
+import BrowseEntertainment from './Pages/BrowseEntertainment/BrowseEntertainment';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' element={<Registration />} />
           <Route path='category' element={<Category />} />
           <Route path='home' element={<Home />} />
+          <Route path='browse' element={<BrowseEntertainment />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/Pages/BrowseEntertainment/BrowseEntertainment.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './BrowseEntertainment.module.css';
+
+export default function BrowseEntertainment() {
+  return (
+    <div className={styles.container}>BrowseEntertainment</div>
+  )
+}
